2383becef0103982ceecf37b470fd77f3c1b54dc
N0ts <mail@n0ts.cn>
2021-10-08 14:25:06 +0800

    ✨[Feat]: 接口封装完毕

---
+++ js/config.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: N0ts
+ * @Date: 2021-10-08 14:14:37
+ * @LastEditTime: 2021-10-08 14:24:05
+ * @Description: 配置
+ * @FilePath: \eazy-gitee-note\js\config.js
+ * @Mail：mail@n0ts.cn
+ */
+
+export default {
+    // 账号
+    owner: "n0ts",
+    // 仓库名称
+    repo: "note",
+    // 分支名
+    sha: "master"
+};
--- js/main.js
+++ js/main.js
@@ -1,26 +1,79 @@
 /*
  * @Author: N0ts
  * @Date: 2021-10-08 00:37:22
- * @LastEditTime: 2021-10-08 01:08:04
+ * @LastEditTime: 2021-10-08 14:23:17
  * @Description: main
  * @FilePath: \eazy-gitee-note\js\main.js
  * @Mail：mail@n0ts.cn
  */
 
+// 从 Vue 中引入
 const { createApp, reactive, toRefs } = Vue;
 
+// 配置文件导入
+import config from "./config.js";
+
+// 数据
 const data = reactive({
     hello: "hello wdnmd"
 });
 
-const App = Vue.createApp({
+// 创建 Vue 应用
+const App = createApp({
+    mounted() {
+        // 获取目录 Tree
+        this.getTrees();
+    },
     setup() {
         return {
             ...toRefs(data)
         };
+    },
+    methods: {
+        /**
+         * 获取目录 Tree
+         */
+        getTrees() {
+            axios
+                .get(`https://gitee.com/api/v5/repos/${config.owner}/${config.repo}/git/trees/${config.sha}`)
+                .then((resData) => {
+                    let result = resData.data;
+                    console.log(result);
+                })
+                .catch((err) => {
+                    console.log("报错啦", err);
+                });
+        },
+        /**
+         * 获取仓库具体路径下的内容
+         * @param {*} path 文件名
+         * @returns 文件数据
+         */
+        getContents(path) {
+            // 数据验证
+            if (!path) {
+                return;
+            }
+
+            // url 转码
+            path = encodeURIComponent(path);
+
+            axios
+                .get(`https://gitee.com/api/v5/repos/${config.owner}/${config.repo}/contents/${path}`)
+                .then((resData) => {
+                    // Base64 解密
+                    resData.data.content = new Buffer(resData.data.content, "base64").toString();
+                    console.log(resData.data);
+                })
+                .catch((err) => {
+                    console.log("报错啦", err);
+                });
+        }
     }
 });
 
+// 使用 ElementUI
 App.use(ElementPlus);
 
-App.mount("#main")
+// 挂载到根节点
+App.mount("#main");

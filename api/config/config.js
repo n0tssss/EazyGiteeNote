c870a3336e77591ad46ad550fa1a6b6bd3acbe33
N0ts <mail@n0ts.cn>
2021-10-09 14:52:59 +0800

    ✨[Feat]: 代码高亮完成

--- api/config/config.js
+++ api/config/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: N0ts
  * @Date: 2021-05-19 02:08:33
- * @LastEditTime: 2021-10-09 01:06:32
+ * @LastEditTime: 2021-10-08 16:47:30
  * @Description: 后端配置
  * @FilePath: \eazy-gitee-note\api\config\config.js
  * @Mail：mail@n0ts.cn
@@ -9,7 +9,7 @@
 
 module.exports = {
     // 私人令牌
-    access_token: "",
+    access_token: "874cb3b8730efb967fde392eea8f1f95",
     // 账号
     owner: "n0ts",
     // 仓库名称

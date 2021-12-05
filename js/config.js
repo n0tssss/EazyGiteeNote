5d0e9a70e6437bae97f7659e3227a3f2ff75cb31
N0ts <mail@n0ts.cn>
2021-12-05 14:24:36 +0800

    📝[Docs]: 删除了多余配置

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: N0ts
  * @Date: 2021-10-08 14:14:37
- * @LastEditTime: 2021-12-05 13:51:14
+ * @LastEditTime: 2021-12-05 14:24:21
  * @Description: 配置
  * @FilePath: /eazy-gitee-note/js/config.js
  * @Mail：mail@n0ts.cn
@@ -17,17 +17,8 @@ export default {
         // 分支名
         sha: "master"
     },
-    gitee: {
-        // 账号
-        owner: "coderlucw",
-        // 仓库名称
-        repo: "note-md",
-        // 分支名
-        sha: "master"
-    },
     // 后端地址
     serverBase: "https://giteeapi.n0ts.cn/10015",
-    serverBase: "https://giteeapi.n0ts.cn/10033",
     // 主题配置
     Themes: ["default-light", "default-dark"]
 };
